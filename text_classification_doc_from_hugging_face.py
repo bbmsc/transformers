adb68eb9eed228314fab4073928a71f2665d675f
bbmsc <andrew.buckler@bbmsc.com>
2024-11-21 16:44:53 -0500

    stuff

--- text_classification_doc_from_hugging_face.py
+++ text_classification_doc_from_hugging_face.py
@@ -1,18 +1,29 @@
 # https://huggingface.co/docs/transformers/tasks/sequence_classification
 
-#from huggingface_hub import notebook_login
-#notebook_login()
+import evaluate
+import numpy as np
 
 from huggingface_hub import login
-login()
 
 from datasets import load_dataset
+
+from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import pipeline
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+
+import torch
+import torch.nn.functional as F
+
+
+
+login()
 
 imdb = load_dataset("imdb")
 
 #imdb["test"][0]
-
-from transformers import AutoTokenizer
 
 #tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
 tokenizer = AutoTokenizer.from_pretrained("./distilbert-base-uncased")
@@ -21,16 +32,9 @@ def preprocess_function(examples):
 
 tokenized_imdb = imdb.map(preprocess_function, batched=True)
 
-from transformers import DataCollatorWithPadding
-
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-import evaluate
-
 accuracy = evaluate.load("accuracy")
-
-import numpy as np
-
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
@@ -39,8 +43,6 @@ def compute_metrics(eval_pred):
 
 id2label = {0: "NEGATIVE", 1: "POSITIVE"}
 label2id = {"NEGATIVE": 0, "POSITIVE": 1}
-
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
 
 model = AutoModelForSequenceClassification.from_pretrained(
     "./distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
@@ -73,17 +75,11 @@ trainer.train()
 
 text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
 
-from transformers import pipeline
-
 classifier = pipeline("sentiment-analysis", model="./my_distilbert")
 classifier(text)
 
-from transformers import AutoTokenizer
-
 tokenizer = AutoTokenizer.from_pretrained("./my_distilbert")
 inputs = tokenizer(text, return_tensors="pt")
-
-from transformers import AutoModelForSequenceClassification
 
 model = AutoModelForSequenceClassification.from_pretrained("./my_distilbert")
 with torch.no_grad():
@@ -91,3 +87,160 @@ with torch.no_grad():
 
 predicted_class_id = logits.argmax().item()
 model.config.id2label[predicted_class_id]
+
+#####
+
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+result = classifier(text)
+print(result)
+
+
+tokens = tokenizer.tokenize(text)
+token_ids = tokenizer.convert_tokens_to_ids(tokens)
+input_ids = tokenizer(text)
+
+print(f' tokens: {tokens}')
+print(f' token ids: {token_ids}')
+print(f' input ids: {input_ids}')
+
+X_train = [text, "we hope you don't hate it."]
+batch = tokenizer(X_train, padding=True, truncation=True, max_length=512, return_tensors="pt")
+print(batch)
+
+with torch.no_grad():
+  outputs = model(**batch, labels=torch.tensor([1, 0]))
+  print(outputs)
+  predictions = F.softmax(outputs.logits, dim=1)
+  print(predictions)
+  labels = torch.argmax(predictions, dim=1)
+  print(labels)
+  labels = [model.config.id2label[label_id] for label_id in labels.tolist()]
+  print(labels)
+
+
+save_directory = "saved"
+tokenizer.save_pretrained(save_directory)
+model.save_pretrained(save_directory)
+
+# then can load
+tokenizer = AutoTokenizer.from_pretrained(save_directory)
+model = AutoModelForSequenceClassification.from_pretrained(save_directory)
+
+
+
+
+# now train a model
+# 1. prepare datasets
+# 2. load pretrained tokenizer, call it with dataset -> encodings
+# 3. build pytorch dataset with encodings
+# 4. load pretrained model
+# 5. a) load huggingface Trainer and train it
+#    b) or use native pytorch trainign pipleine.
+
+model_name = "distilbert-base-uncased"
+
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import torch
+from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+from torch.utils.data import DataLoader, Dataset
+from transformers import DistilBertTokenizerFast, AdamW
+
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir/label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir is "neg" else 1)
+    return texts, labels
+
+# now get seom data; for example, large m ovie review dataset
+# http://ai.stanford.edu/~amaas/data/sentiment
+train_texts, train_labels = read_imdb_split('aclImdb/train')
+test_texts, test_labels = read_imdb_split('aclImdb/test')
+
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+class IMDbDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+tokenizer = DistilBertTokenizerFast.from_pretrained(model_name)
+
+# ensure that all of our sequences are padded to the same length and are truncated to be no longer than the model's
+# maximum input length. this will allow us to feed batches of sequences int the model at the same time.
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+val_encodings = tokenizer(val_texts, truncation=True, padding=True)
+test_encodings = tokenizer(test_texts, truncation=True, padding=True)
+
+train_dataset = IMDbDataset(train_encodings, train_labels)
+val_dataset = IMDbDataset(val_encodings, val_labels)
+test_dataset = IMDbDataset(test_encodings, test_labels)
+
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=2,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    learning_rate=5e-5,              # learning rate
+    weight_decay=0.01,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
+
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset             # evaluation dataset
+)
+
+trainer.train() # from huggingface
+
+# or native pytrch
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+model.to(device)
+model.train()
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+optim = AdamW(model.parameters(), lr=5e-5)
+
+num_train_epochs=2
+for epoch in range(num_train_epochs):
+    for batch in train_loader:
+        optim.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        
+        loss = outputs[0]
+        loss.backward()
+        optim.step()
+
+model.eval()
+
+# end native pytorch
