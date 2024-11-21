a9e7566a133be603b820d505ae6f4e82878daa3d
bbmsc <andrew.buckler@bbmsc.com>
2024-11-21 09:08:40 -0500

    add first script

---
+++ text_classification_doc_from_hugging_face.py
@@ -0,0 +1,89 @@
+# https://huggingface.co/docs/transformers/tasks/sequence_classification
+
+#from huggingface_hub import notebook_login
+#notebook_login("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")
+
+from datasets import load_dataset
+
+imdb = load_dataset("imdb")
+
+#imdb["test"][0]
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+
+from transformers import DataCollatorWithPadding
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+import evaluate
+
+accuracy = evaluate.load("accuracy")
+
+import numpy as np
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    "distilbert/distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+)
+
+training_args = TrainingArguments(
+    output_dir="my_awesome_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    processing_class=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis", model="bbmsc/my_awesome_model")
+classifier(text)
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("bbmsc/my_awesome_model")
+inputs = tokenizer(text, return_tensors="pt")
+
+from transformers import AutoModelForSequenceClassification
+
+model = AutoModelForSequenceClassification.from_pretrained("bbmsc/my_awesome_model")
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+predicted_class_id = logits.argmax().item()
+model.config.id2label[predicted_class_id]
